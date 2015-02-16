48d54c4410558362c3846617b1fe3f2bb5c9b0d6
jarm14 <joelram1@hotmail.com>
2015-02-16 17:03:59 -0500

    question
    getters and setters

--- Questions_Patient_Doctor/question.h
+++ Questions_Patient_Doctor/question.h
@@ -8,100 +8,100 @@ using namespace std;
 class question{
 
 	private:
-		
+
 		int id_question;
 		int id_doctor;
 		int id_patient;
-		char date[18];
-		char text[1024];
-		char context[2048];
-		char answer[2048];
-		char comment[2048];
+		char *date;
+		char *text;
+		char *context;
+		char *answer;
+		char *comment;
 
 	public:
 
+		question(){
+		id_question = 0;
+		id_doctor = 0;
+		id_patient = 0;
+		date = new char[18];
+		text = new char[1024];
+		context = new char[2048];
+		answer = new char[2048];
+		comment = new char[2048];
+		}
+
 		void setIdQuestion(int id){
-		
+
 			id_question = id;
 		}
 
 		int getIdQuestion(){
-		
+
 			return id_question;
 		}
 
 		void setIdDoctor(int id){
-		
+
 			id_doctor = id;
 		}
 
 		int getIdDoctor(){
-		
+
 			return id_doctor;
 		}
 
 		void setIdPatient(int id){
-		
+
 			id_patient = id;
 		}
 
 		int getIdPatient(){
-		
+
 			return id_patient;
 		}
 
 		void setDate(char dat[18]){
-		
 			strcpy(date,dat);
 		}
 
 		char *getDate(){
-		
-			char *dat=date;
-			return dat;
+			return date;
 		}
 
 		void setContext(char cont[2048]){
-		
+
 			strcpy(context,cont);
 		}
 
 		char *getContext(){
-		
-			char *cont=context;
-			return cont;
+			return context;
 		}
 
 		void setText(char tex[1024]){
-		
+
 			strcpy(text,tex);
 		}
 
 		char *getText(){
-		
-			char *tex=text;
-			return tex;
+			return text;
 		}
 
 		void setAnswer(char answ[2048]){
-		
+
 			strcpy(answer,answ);
 		}
 
 		char *getAnswer(){
-		
-			char *answ=answer;
-			return answ;
+			return answer;
 		}
 
 		void setComment(char comm[2048]){
-		
+
 			strcpy(comment,comm);
 		}
 
 		char *getComent(){
-		
-			char *comm=comment;
-			return comm;
+			return comment;
 		}
 };
