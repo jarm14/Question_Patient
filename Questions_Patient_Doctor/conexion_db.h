5eddfedf165706084dc0d4a5110fe35a27886288
jarm14 <joelram1@hotmail.com>
2015-02-16 09:40:15 -0500

    connexion
    improve connexion class

--- Questions_Patient_Doctor/conexion_db.h
+++ Questions_Patient_Doctor/conexion_db.h
@@ -15,14 +15,78 @@ class connexion{
 		SQLCHAR sqlstate[1024];
 		SQLCHAR message[1024];
 		if(SQL_SUCCESS == SQLGetDiagRec(handletype, handle, 1, sqlstate, NULL, message, 1024, NULL)){}
-			cout<<"Alerta: "<<message<<"\nSQLSTATE: "<<sqlstate<<endl;		
+			//cout<<"Alerta: "<<message<<"\nSQLSTATE: "<<sqlstate<<endl;		
 	}
 
 public:
 
-	question *getQuestion(){
+	int getCount(){
+		
+		int count;
+		SQLHANDLE sqlenvhandle;    
+		SQLHANDLE sqlconnectionhandle;
+		SQLHANDLE sqlstatementhandle;
+		SQLRETURN retcode;
 
+		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlenvhandle))
+			goto FINISHED;
+
+		if(SQL_SUCCESS!=SQLSetEnvAttr(sqlenvhandle,SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)) 
+			goto FINISHED;
+    
+		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_DBC, sqlenvhandle, &sqlconnectionhandle))
+			goto FINISHED;
+
+		SQLCHAR retconstring[1024];
+		switch(SQLDriverConnect (sqlconnectionhandle, 
+					NULL, 
+					(SQLCHAR*)"Driver={SQL Server Native Client 10.0};Server=tcp:ogt4jiumex.database.windows.net,1433;Database=espe;Uid=espe@ogt4jiumex;Pwd={Admin112358.};Encrypt=yes;Connection Timeout=30;", 
+					SQL_NTS, 
+					retconstring, 
+					1024, 
+					NULL,
+					SQL_DRIVER_NOPROMPT)){
+			case SQL_SUCCESS_WITH_INFO:
+				show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+				break;
+			case SQL_INVALID_HANDLE:
+			case SQL_ERROR:
+				show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+				goto FINISHED;
+			default:
+				break;
+		}
+    
+		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_STMT, sqlconnectionhandle, &sqlstatementhandle))
+			goto FINISHED;
+
+		if(SQL_SUCCESS!=SQLExecDirect(sqlstatementhandle, (SQLCHAR*)"select count(*) from question", SQL_NTS)){
+			show_error(SQL_HANDLE_STMT, sqlstatementhandle);
+			goto FINISHED;
+		}
+		else{
+			
+			if(SQLFetch(sqlstatementhandle)==SQL_SUCCESS){//Fetch datos de base de datos
+				SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &count, 0, NULL);
+			
+	
+			}
+		}
+
+	FINISHED:
+		SQLFreeHandle(SQL_HANDLE_STMT, sqlstatementhandle );
+		SQLDisconnect(sqlconnectionhandle);
+		SQLFreeHandle(SQL_HANDLE_DBC, sqlconnectionhandle);
+		SQLFreeHandle(SQL_HANDLE_ENV, sqlenvhandle);
+
+		return count;
+	}
+
+	question *getQuestion(){
+		int count;
+		count=getCount();
 		question *_question;
+		_question= new question[count];
 		SQLHANDLE sqlenvhandle;    
 		SQLHANDLE sqlconnectionhandle;
 		SQLHANDLE sqlstatementhandle;
@@ -65,6 +129,7 @@ public:
 			goto FINISHED;
 		}
 		else{
+			int i=0;//recorre el array de question
 			int id_question;
 			int id_doctor;
 			int id_patient;
@@ -73,7 +138,6 @@ public:
 			char context[2048];
 			char answer[2048];
 			char comment[2048];
-			int cont=0;
 			while(SQLFetch(sqlstatementhandle)==SQL_SUCCESS){//Fetch datos de base de datos
 				SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &id_question, 0, NULL);
 				SQLGetData(sqlstatementhandle, 2, SQL_C_ULONG, &id_doctor, 0, NULL);
@@ -83,42 +147,19 @@ public:
 				SQLGetData(sqlstatementhandle, 6, SQL_C_CHAR, context, 2048, NULL);
 				SQLGetData(sqlstatementhandle, 7, SQL_C_CHAR, answer, 2048, NULL);
 				SQLGetData(sqlstatementhandle, 8, SQL_C_CHAR, comment, 2048, NULL);
-				cont++;
 				
-			}
-
-			_question= new question[cont];
-			int i=0;
-			
-			if(SQL_SUCCESS!=SQLExecDirect(sqlstatementhandle, (SQLCHAR*)"select * from question", SQL_NTS)){
-				show_error(SQL_HANDLE_STMT, sqlstatementhandle);
-				goto FINISHED;
-			}
-			else{
-				
-				while(SQLFetch(sqlstatementhandle)==SQL_SUCCESS){//Fetch datos de base de datos
-					SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &id_question, 0, NULL);
-					SQLGetData(sqlstatementhandle, 2, SQL_C_ULONG, &id_doctor, 0, NULL);
-					SQLGetData(sqlstatementhandle, 3, SQL_C_ULONG, &id_patient, 0, NULL);
-					SQLGetData(sqlstatementhandle, 4, SQL_C_CHAR, date, 18, NULL);
-					SQLGetData(sqlstatementhandle, 5, SQL_C_CHAR, text, 1024, NULL);
-					SQLGetData(sqlstatementhandle, 6, SQL_C_CHAR, context, 2048, NULL);
-					SQLGetData(sqlstatementhandle, 7, SQL_C_CHAR, answer, 2048, NULL);
-					SQLGetData(sqlstatementhandle, 8, SQL_C_CHAR, comment, 2048, NULL);
-				
-					//Guardar datos en objeto
-					_question[i].setIdQuestion(id_question);
-					_question[i].setIdDoctor(id_doctor);
-					_question[i].setIdPatient(id_patient);
-					_question[i].setDate(date);
-					_question[i].setText(text);
-					_question[i].setContext(context);
-					_question[i].setAnswer(answer);
-					_question[i].setComment(comment);
+				//Guardar datos en objeto
+				_question[i].setIdQuestion(id_question);
+				_question[i].setIdDoctor(id_doctor);
+				_question[i].setIdPatient(id_patient);
+				_question[i].setDate(date);
+				_question[i].setText(text);
+				_question[i].setContext(context);
+				_question[i].setAnswer(answer);
+				_question[i].setComment(comment);
+				i++;
 	
-				}
 			}
-		
 		}
 
 	FINISHED:
@@ -128,4 +169,6 @@ public:
 		SQLFreeHandle(SQL_HANDLE_ENV, sqlenvhandle);
 		return _question;
 	}
+
+
 };
